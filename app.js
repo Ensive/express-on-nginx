a81065ea36edc774f0d45e18e0f31e73408ee556
Anton Honcharuk <anton.goncharuk@icloud.com>
2019-10-04 21:12:14 +0300

    Send customer header

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const port = 3000;
 
 app.use(compression());
 
-app.get('/robots.txt', function (req, res) {
-    res.type('text/plain');
-    res.send("User-agent: *\nDisallow: /");
+app.get('/robots.txt', function(req, res) {
+  res.type('text/plain');
+  res.send('User-agent: *\nDisallow: /');
 });
 
 app.get('/', (req, res) => {
-	res.send('This one runs an express server!');
+  res.send('This one runs an express server!');
+});
+
+app.get('/demo', (req, res) => {
+  res.set('X-full-stack', '4life');
+  res.status(418);
+  res.send('I prefer coffee');
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
